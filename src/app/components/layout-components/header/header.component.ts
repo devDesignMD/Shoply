daaf446b179f8640de945cd6a6e277926fddad50
devDesignMD <82402719+devDesignMD@users.noreply.github.com>
2022-12-06 22:24:37 +0300

    tailwind configuration

--- src/app/components/layout-components/header/header.component.ts
+++ src/app/components/layout-components/header/header.component.ts
@@ -4,8 +4,7 @@ import { PageRoutes } from '../../../ts/enum';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent  {
   readonly links: LinkInterface[] = [
