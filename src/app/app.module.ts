93e651a12f8e7c11c88c7b1b1728aba4391be8d6
devDesignMD <82402719+devDesignMD@users.noreply.github.com>
2022-12-08 00:50:02 +0300

    added header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BaseRoutingModule } from './router/base-routing.module';
 import {RouterModule} from "@angular/router";
+import { PartnersComponent } from './components/partners/partners.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PartnersComponent],
   imports: [BrowserModule, BaseRoutingModule,  RouterModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [
+    PartnersComponent
+  ],
 })
 export class AppModule {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {HomeComponentsModule} from "../../components/home-components/home-components.module";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  imports: [CommonModule, HomeComponentsModule],
+  templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
 
