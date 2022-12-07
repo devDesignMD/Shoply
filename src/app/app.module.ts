187db73222cc7cb668aabc33557288b9772c59bf
devDesignMD <82402719+devDesignMD@users.noreply.github.com>
2022-12-08 01:28:36 +0300

    added header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BaseRoutingModule } from './router/base-routing.module';
 import {RouterModule} from "@angular/router";
-import { PartnersComponent } from './components/partners/partners.component';
 
 @NgModule({
-  declarations: [AppComponent, PartnersComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, BaseRoutingModule,  RouterModule],
   providers: [],
   bootstrap: [AppComponent],
   exports: [
-    PartnersComponent
   ],
 })
 export class AppModule {}
--- src/app/components/home-components/home-components.module.ts
+++ src/app/components/home-components/home-components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
+import {PartnersComponent} from "../partners/partners.component";
 
 @NgModule({
-  declarations: [ HeroComponent],
+  declarations: [ HeroComponent,PartnersComponent],
   imports: [CommonModule],
   exports: [ HeroComponent],
 })
