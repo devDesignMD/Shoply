bef9d66cb47fa33206436d129c00b11302689906
devDesignMD <82402719+devDesignMD@users.noreply.github.com>
2022-12-11 18:12:39 +0300

    added login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { BaseModule } from '../../components/base/base.module';
 import { FormInputComponent } from '../../components/base/form-input/form-input.component';
 import { PageRoutes } from '../../ts/enum';
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, BaseModule, FormInputComponent],
+  imports: [CommonModule, BaseModule, FormInputComponent,RouterModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
