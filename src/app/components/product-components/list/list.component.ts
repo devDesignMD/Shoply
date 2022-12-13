9f835b18ac2be1571758c052704529592e251232
devDesignMD <82402719+devDesignMD@users.noreply.github.com>
2022-12-13 21:41:50 +0300

    added products-list page

---
+++ src/app/components/product-components/list/list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'List',
+  templateUrl: './list.component.html',
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
